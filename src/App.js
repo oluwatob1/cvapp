20e1816f83c9bbe0f374ade8f6f167a73ec14b5a
Ayomide Olanrewaju <distinctshoot01@gmail.com>
2022-04-18 09:31:08 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+// import './App.css';
+import Eduexp from './Components/EduExp/Eduexp';
+import Geninfo from './Components/Gen_Info/Geninfo';
+import Practexp from './Components/PractExp/Practexp';
+
+function App() {
+  return (
+    <div className="App">
+      <Geninfo />
+      <Eduexp />
+      <Practexp />
+    </div>
+  );
+}
+
+export default App;
